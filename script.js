bbfd98048264c3e9ab8563f3a6f1a8c14eed52df
Nilo Venturin <147455775+dev-nilo@users.noreply.github.com>
2025-04-14 23:53:22 +0000

    todo now with localStorage

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("taskForm");
   const input = document.getElementById("taskInput");
   const taskList = document.getElementById("taskList");
 
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
+  function saveTasks() {
+    const tasks = [];
+
+    taskList.querySelectorAll("li p").forEach((p) => tasks.push(p.textContent));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+
+  function loadTasks() {
+    const tasksStr = localStorage.getItem("tasks");
+
+    if (tasksStr) {
+      const tasks = JSON.parse(tasksStr);
+
+      tasks.forEach((taskText) => addTask(taskText));
+    }
+  }
+
+  function addTask(taskText) {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    const img = document.createElement("img");
+
+    p.textContent = taskText;
+    img.src = "assets/trash-solid.svg";
+    img.alt = "Delete task";
+
+    li.appendChild(p);
+    li.appendChild(img);
+    taskList.appendChild(li);
+
+    img.addEventListener("click", () => {
+      taskList.removeChild(li);
+
+      saveTasks();
+    });
+  }
+
+  loadTasks();
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
     const taskText = input.value;
-
     if (taskText !== "") {
-      const li = document.createElement("li");
-      const p = document.createElement("p");
-      const img = document.createElement("img");
-
-      p.textContent = taskText;
-      img.src = "assets/trash-solid.svg";
-      img.alt = "Delete task";
-
-      li.appendChild(p);
-      li.appendChild(img);
-
-      taskList.appendChild(li);
+      addTask(taskText);
       input.value = "";
-
-      img.addEventListener("click", () => {
-        taskList.removeChild(li);
-      });
+      saveTasks();
     }
   });
 });
